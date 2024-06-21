5aedaba918f59f2cfc82d6a66b483b8fac0ba598
Haritiana Adriano <hei.adriano.4@gmail.com>
2024-06-21 11:11:41 +0300

    infra: enable logging info and error, api documentation auto generated

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: ['error', 'warn', 'log', 'verbose', 'fatal'],
+  });
+
+  const config = new DocumentBuilder()
+    .setTitle('Personal Blog api')
+    .setDescription('Simple project to mastering nest')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  app.enableCors();
   await app.listen(3000);
 }
+
 bootstrap();
