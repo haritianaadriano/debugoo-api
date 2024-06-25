15f429271332011a1238d45ab87bdfb7d7414815
Haritiana Adriano <hei.adriano.4@gmail.com>
2024-06-25 09:08:17 +0300

    feat: post resources and controller

---
+++ src/controller/post.controller.ts
@@ -0,0 +1,96 @@
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Put,
+  Query,
+  UseGuards,
+} from '@nestjs/common';
+import { AuthGuard } from '../auth/guards/auth.guards';
+import { PostService } from '../service/post.service';
+import { CreatePostApi, PostApi } from './api/post.rest';
+import { ApiBody, ApiCreatedResponse, ApiTags } from '@nestjs/swagger';
+import { PaginationQuery } from './queries/pagination.query';
+import { PostMapper } from './mapper/post.mapper';
+
+@Controller()
+export class PostController {
+  constructor(
+    private readonly postService: PostService,
+    private readonly postMapper: PostMapper,
+  ) {}
+
+  @UseGuards(AuthGuard)
+  @Get('/posts')
+  @ApiCreatedResponse({
+    description: 'Posts found',
+    type: PostApi,
+    isArray: true,
+  })
+  @ApiTags('Posting')
+  async findPosts(
+    @Query() paginationQuery: PaginationQuery,
+  ): Promise<PostApi[]> {
+    const projects = await this.postService.findPosts(paginationQuery);
+    const mappedProjects = await Promise.all(
+      projects.map((project) => this.postMapper.fromDomainToRest(project)),
+    );
+    return mappedProjects;
+  }
+
+  @UseGuards(AuthGuard)
+  @Get('/posts/:post_id')
+  @ApiCreatedResponse({
+    description: 'Post found',
+    type: PostApi,
+  })
+  @ApiTags('Posting')
+  async findPostsById(@Param('post_id') postId: string): Promise<PostApi> {
+    return this.postMapper.fromDomainToRest(
+      await this.postService.findPostById(postId),
+    );
+  }
+
+  @UseGuards(AuthGuard)
+  @Get('/users/:user_id/posts')
+  @ApiCreatedResponse({
+    description: 'Posts found from given user',
+    type: PostApi,
+    isArray: true,
+  })
+  @ApiTags('Posting')
+  async findUserPosts(
+    @Param('user_id') userId: string,
+    @Query() pagination: PaginationQuery,
+  ): Promise<PostApi[]> {
+    const posts = await this.postService.findPostsByUserId(pagination, userId);
+    const mappedPosts = await Promise.all(
+      posts.map((post) => this.postMapper.fromDomainToRest(post)),
+    );
+    return mappedPosts;
+  }
+
+  @UseGuards(AuthGuard)
+  @Put('/users/:user_id/posts')
+  @ApiCreatedResponse({
+    description: 'Post created',
+    type: PostApi,
+    isArray: true,
+  })
+  @ApiBody({
+    isArray: true,
+    type: CreatePostApi,
+  })
+  @ApiTags('Posting')
+  async saveUserPosts(
+    @Param('user_id') userId: string,
+    @Body() projectsApi: CreatePostApi[],
+  ): Promise<PostApi[]> {
+    const posts = await this.postService.savePosts(projectsApi, userId);
+    const mappedPosts = await Promise.all(
+      posts.map((project) => this.postMapper.createdRest(project)),
+    );
+    return mappedPosts;
+  }
+}
--- src/service/post.service.ts
+++ src/service/post.service.ts
@@ -1,10 +1,10 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Inject, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Post } from '../model/post.entity';
 import { Repository } from 'typeorm';
 import { PaginationQuery } from '../controller/queries/pagination.query';
 import { UserService } from './user.service';
-import { CreatePostApi } from 'src/controller/api/post.rest';
+import { CreatePostApi } from '../controller/api/post.rest';
 
 @Injectable()
 export class PostService {
@@ -15,7 +15,7 @@ export class PostService {
     private readonly userService: UserService,
   ) {}
 
-  async findProjects(pagination: PaginationQuery): Promise<Post[]> {
+  async findPosts(pagination: PaginationQuery): Promise<Post[]> {
     const { page, page_size } = pagination;
     const skip = (page - 1) * page_size;
 
@@ -25,11 +25,33 @@ export class PostService {
     });
   }
 
+  async findPostsByUserId(
+    pagination: PaginationQuery,
+    userId: string,
+  ): Promise<Post[]> {
+    const { page, page_size } = pagination;
+    const skip = (page - 1) * page_size;
+
+    return this.postRepository
+      .createQueryBuilder('post')
+      .leftJoinAndSelect('post.user', 'user')
+      .where('post.user = :userId', { userId })
+      .skip(skip)
+      .take(page_size)
+      .getMany();
+  }
+
   async findPostById(id: string): Promise<Post | undefined> {
     return this.postRepository.findOneById(id);
   }
 
-  async savePosts(toCreatePost: CreatePostApi[]): Promise<Post[] | undefined> {
+  async savePosts(
+    toCreatePost: CreatePostApi[],
+    userId: string,
+  ): Promise<Post[] | undefined> {
+    toCreatePost.forEach((post) =>
+      this.validateUserPathAndPayload(post, userId),
+    );
     return this.postRepository.save(
       await this.fromCreateToDomain(toCreatePost),
     );
@@ -50,5 +72,14 @@ export class PostService {
         return this.mapCreateToDomainPost(project);
       }),
     );
+  }
+
+  validateUserPathAndPayload(payload: CreatePostApi, userId: string) {
+    if (payload.user_id != userId) {
+      throw new HttpException(
+        'User path doesn t match to user payload',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
   }
 }
