1972fe4bb61a546c40047caac55ee9d20320239f
Haritiana Adriano <103512767+haritianaadriano@users.noreply.github.com>
2024-06-25 09:30:30 +0300

    feat: user controller to fetch users

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -1,7 +1,55 @@
-import { Controller } from '@nestjs/common';
-import { UserService } from 'src/service/user.service';
+import {
+  Controller,
+  Get,
+  Param,
+  Query,
+  UseGuards,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { UserService } from '../service/user.service';
+import { UserMapper } from './mapper/user.mapper';
+import { AuthGuard } from '../auth/guards/auth.guards';
+import { ApiCreatedResponse, ApiTags } from '@nestjs/swagger';
+import { UserApi } from './api/user.rest';
+import { PaginationQuery } from './queries/pagination.query';
 
 @Controller()
 export class UserController {
-  constructor(private readonly userService: UserService) {}
+  constructor(
+    private readonly userService: UserService,
+    private readonly userMapper: UserMapper,
+  ) {}
+
+  @UseGuards(AuthGuard)
+  @Get('/users')
+  @ApiCreatedResponse({
+    description: 'Users found',
+    type: UserApi,
+    isArray: true,
+  })
+  @ApiTags('Users')
+  @UsePipes(new ValidationPipe())
+  async findUsers(
+    @Query() paginationQuery: PaginationQuery,
+  ): Promise<UserApi[]> {
+    const users = await this.userService.findUsers(paginationQuery);
+    const mappedUsers = await Promise.all(
+      users.map((user) => this.userMapper.fromDomainToRest(user)),
+    );
+    return mappedUsers;
+  }
+
+  @UseGuards(AuthGuard)
+  @Get('/users/:id')
+  @ApiCreatedResponse({
+    description: 'User found',
+    type: UserApi,
+  })
+  @ApiTags('Users')
+  async findUserById(@Param('id') id: string): Promise<UserApi> {
+    const user = await this.userService.findUserById(id);
+    const mappedUser = this.userMapper.fromDomainToRest(user);
+    return mappedUser;
+  }
 }
--- src/service/user.service.ts
+++ src/service/user.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from '../model/user.entity';
 import { Repository } from 'typeorm';
+import { PaginationQuery } from '../controller/queries/pagination.query';
 
 @Injectable()
 export class UserService {
@@ -9,6 +10,16 @@ export class UserService {
     @InjectRepository(User)
     private readonly userRepository: Repository<User>,
   ) {}
+
+  async findUsers(pagination: PaginationQuery): Promise<User[]> {
+    const { page, page_size } = pagination;
+    const skip = (page - 1) * page_size;
+
+    return this.userRepository.find({
+      take: page_size,
+      skip,
+    });
+  }
 
   async findUserByEmail(email: string): Promise<User | undefined> {
     return this.userRepository.findOneBy({ email: email as string });
